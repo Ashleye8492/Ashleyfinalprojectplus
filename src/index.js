d0039aaa72b00e2b1126871ac588fe46d0fb2037
Ashleye8492 <113311526+Ashleye8492@users.noreply.github.com>
2024-01-02 19:26:42 -0600

    Created .json file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
